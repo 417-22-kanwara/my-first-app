286ad87e773bcb508d77e74b7375044c68c321b8
417-22-kanwara <56416@yupparaj.ac.th>
2026-09-16 11:29:35 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -52,14 +52,14 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     # ตรวจข้อ 3
-    if u_ans3 == "Cherry":
+    if u_ans3 == "cherry":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
     # ตรวจข้อ 4
-    if u_ans4 == "Coconut":
+    if u_ans4 == "coconut":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
